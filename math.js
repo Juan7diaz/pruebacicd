8fba4730dd970ca7f8b1b3662f82b452a9804bf8
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-10-10 15:50:45 -0500

    prueba cambiando division

--- math.js
+++ math.js
@@ -4,7 +4,7 @@ const resta = (a, b) => a - b;
 const multiplicacion = (a, b) => a * b;
 const division = (a, b) => {
 
-  if(b === 0) return "No se puede dividir por 0";
+  if(b === 0) return "No se puede dividir por 1";
   //devuelve un numero con 2 decimales
   return parseFloat((a / b).toFixed(2));
 };
